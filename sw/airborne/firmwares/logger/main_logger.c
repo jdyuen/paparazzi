41e188ba4892a5a83a4eafc1a358bfc567061cbd
Jamie Yuen <jdyuen@ualberta.ca>
2012-06-20 10:02:01 -0600

    added OO logging for UART1

--- sw/airborne/firmwares/logger/main_logger.c
+++ sw/airborne/firmwares/logger/main_logger.c
@@ -545,7 +545,7 @@ int do_log(void)
 //			LED_TOGGLE(LED_GREEN);
 			inc = Uart1Getch();
 #ifdef LOG_OO
-            log_oo(inc, LOG_SOURCE_UART0);
+            log_oo(inc, LOG_SOURCE_UART1);
 #else
 #ifdef LOG_XBEE
             log_xbee(inc, LOG_SOURCE_UART1);
